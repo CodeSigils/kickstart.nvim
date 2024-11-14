6beb7f4ef934dd4cd5eef1eefdd23182a9546546
CodeSigils <web_tools88@yahoo.com>
2024-11-14 13:41:54 +0200

    better lua examples

--- notes/lua-prime.lua
+++ notes/lua-prime.lua
@@ -1,60 +1,100 @@
--- https://learnxinyminutes.com/docs/lua/
+-- https://nvchad.com/docs/quickstart/learn-lua-
+-- https://www.tutorialspoint.com/lua/lua_nested_loops.htm
 
-----------------------------------------------------
--- 1. Variables and flow control.
-----------------------------------------------------
+-- ----------------------------------------------------
+-- -- 1. Arrays
+-- ----------------------------------------------------
 
--- myGlobalVar = 'Attention this is global'
--- print(myGlobalVar)
-local num = 12
+local line = '\n---------------\n'
 
-if num > 40 then
-  print 'over 40'
-elseif num == 40 then
-  io.write 'is 40\n'
-else
-  local line = '.........'
-  print('Winter is coming, ' .. line)
+local colors = { 'red', 'green', 'blue' }
+
+print(line .. 'Loop through Array using for loop' .. line)
+for i = 1, #colors do
+  print(colors[i])
 end
 
--- Undefined variables return nil.
--- This is not an error:
-
--- local foo = anUnknownVariable -- Now foo = nil.
--- print(foo)
-
-local aBoolvalue = false
-
-if not aBoolvalue then
-  print 'it is false'
+-- Using ipairs
+print(line .. 'Loop through Array using ipairs' .. line)
+for index, value in ipairs(colors) do
+  print(colors[index], value)
 end
 
--- In general, the range is begin, end[, step].
+-- Without assigning  a variable
+print(line .. 'Loop through Array using for loop without index' .. line)
+for _, value in ipairs(colors) do
+  print(value)
+end
 
--- Another loop construct:
-repeat
-  print 'the way of the future'
-  num = num - 1
-until num == 0
+-- ----------------------------------------------------
+-- -- 2. Dictionaries
+-- ----------------------------------------------------
 
-----------------------------------------------------
--- 2. Functions.
-----------------------------------------------------
-local function fib(n)
-  if n < 2 then
-    return 1
+-- Dictionaries contain key-value pairs
+
+local info = {
+  name = 'Sid',
+  age = 20,
+  isAdmin = true,
+}
+
+-- Two ways to print a value:
+print(line .. 'Two methods to get values - Dot notation support' .. line)
+print(info['name'])
+print(info.name)
+
+-- Loop by pairs
+print(line .. 'Get dictionary values using key value and pairs() method in a for loop' .. line)
+for key, value in pairs(info) do
+  print(key .. ' ' .. tostring(value))
+end
+
+-- ----------------------------------------------------
+-- -- 3. Nested Tables
+-- ----------------------------------------------------
+
+-- Nested list
+local data = {
+  { 'Sid', 20 },
+  { 'Tim', 90 },
+}
+
+for i = 1, #data do
+  print(data[i][1] .. ' is ' .. data[i][2] .. ' years old')
+end
+
+-- Nested dictionaries
+local users = {
+  sid = { age = 20 },
+  tim = { age = 90 },
+}
+
+-- print(users) -- Prints table's memory location
+
+print(line)
+
+-- use pairs keyword
+for name, user in pairs(users) do
+    print("User " .. name .. "'s age is " .. user["age"])
+end
+
+print(line)
+
+-- Use a recursive function to get Nested values wit `pairs` keyword
+function DeepPrint(e)
+  -- if e is a table, we should iterate over its elements
+  if type(e) == 'table' then
+    for k, v in pairs(e) do -- for every element in the table
+      print(k)
+      DeepPrint(v) -- recursively repeat the same procedure
+    end
+  else -- if not, we can just print it
+    print(e)
   end
-  return fib(n - 2) + fib(n - 1)
 end
 
-print('Fibonnaci ' .. fib(6))
+DeepPrint(users)
 
--- x = 1, y = 2, z = 3 and 4 is thrown away
-x, y, z = 1, 2, 3, 4
-
-local function bar(a, b, c)
-  print(a, b, c)
-  return 4, 8, 15, 16, 23, 42
-end
-
-x, y = bar 'zaphod' --- zaphod nil nil
+-- ----------------------------------------------------
+-- -- 4. Tables
+-- ----------------------------------------------------
